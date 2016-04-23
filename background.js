7c6c0bf389f33f55ed869a6bc432e9c12d28de2c
Alex Sharov <kvendingoldo@yandex.ru>
2016-04-23 13:06:40 +0300

    [VB] added lost background.json

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onMessage.addListener(
+ 	function(request, sender, sendResponse) {
+		var a = request.site; // data about site
+		var b = request.time; // data about time on site
+  });
